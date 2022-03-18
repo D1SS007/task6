a763a8afcf860e9b256c31ea9afe4c5391338711
D1SS <D1SS@DESKTOP-2QR7GQG>
2022-03-18 18:48:00 +0300

    Добавьте файлы проекта.

---
+++ task6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfPictures = 52;
+            int picturesInRow = 3;
+
+            int fullRows;
+            int picturesRemains;
+
+            fullRows = amountOfPictures / picturesInRow;
+            picturesRemains = amountOfPictures % picturesInRow;
+
+            Console.WriteLine("Полных рядов = "  + fullRows + " " + ", картинок осталось =  "+ picturesRemains);
+
+
+
+        }
+    }
+}
